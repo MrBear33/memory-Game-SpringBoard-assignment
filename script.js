164f25def6a2efa3ba44b978a822f93227fb7ed5
mrbear <mrbear@DESKTOP-V64KSDE>
2024-05-29 18:46:05 -0500

    Adding memory game to Github

---
+++ script.js
@@ -0,0 +1,116 @@
+const gameContainer = document.getElementById("game");
+const startButton = document.getElementById('startButton');
+const resetButton = document.getElementById('resetButton');
+let card1 = null;
+let card2 = null;
+let cardsFlipped = 0;
+let noClicking = false;
+
+
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple"
+];
+
+
+// startButton.addEventListener('click', startGame);
+// resetButton.addEventListener('click', resetGame);
+
+function startGame () {
+  startButton.addEventListener('click', startGame);
+ 
+  gameContainer.style.display = 'inline-block';
+}
+
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+let shuffledColors = shuffle(COLORS);
+
+// this function loops over the array of colors
+// it creates a new div and gives it a class with the value of the color
+// it also adds an event listener for a click for each card
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add(color);
+    newDiv.addEventListener("click", handleCardClick);
+    gameContainer.append(newDiv);
+  }
+}
+
+function handleCardClick(e) {
+ 
+  if (noClicking) return;
+  if (e.target.classList.contains("flipped")) return;
+
+  let currentCard = e.target;
+  currentCard.style.backgroundColor = currentCard.classList[0];
+
+  // checking if cards have been flipped and adding class flipped
+  if (!card1 || !card2) {
+    currentCard.classList.add("flipped");
+    card1 = card1 || currentCard;
+    card2 = currentCard === card1 ? null : currentCard;
+  }
+// comparing 2 flipped cards
+  if (card1 && card2) {
+    noClicking = true;
+   
+    let gif1 = card1.className;
+    let gif2 = card2.className;
+
+    if (gif1 === gif2) {
+      cardsFlipped += 2;
+      card1.removeEventListener("click", handleCardClick);
+      card2.removeEventListener("click", handleCardClick);
+      card1 = null;
+      card2 = null;
+      noClicking = false;
+    } else {
+      setTimeout(function() {
+        card1.style.backgroundColor = "";
+        card2.style.backgroundColor = "";
+        card1.classList.remove("flipped");
+        card2.classList.remove("flipped");
+        card1 = null;
+        card2 = null;
+        noClicking = false;
+      }, 1000);
+    }
+  }
+
+  // setTimeout so that all colors get filled before the alert message pops up
+  if (cardsFlipped === COLORS.length) {
+    setTimeout(function (){
+   alert("game over!");
+  }, 500);
+}
+}
+
+createDivsForColors(shuffledColors);
